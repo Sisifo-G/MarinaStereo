5612cf40b2cc14f536224a90f54a46b86fce3e61
Manuel Perdomo <manuelperdomo1710@icloud.com>
2019-07-13 06:34:12 -0500

    Punto crítico

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,56 @@
 
 $(document).ready(function() {
 
-  // Smooth scrolling
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, 'easeInOutExpo');
-
-          if ( $(this).parents('.nav-menu').length ) {
-            $('.nav-menu .menu-active').removeClass('menu-active');
-            $(this).closest('li').addClass('menu-active');
-          }
-
-          if ( $('body').hasClass('mobile-nav-active') ) {
-              $('body').removeClass('mobile-nav-active');
-              $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-              $('#mobile-body-overly').fadeOut();
-          }
-          return false;
-        }
+    // Header fixed and Back to top button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+        $('#header').addClass('header-fixed');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+        $('#header').removeClass('header-fixed');
       }
     });
+    $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+      return false;
+    });
+
+   
+
+  // Smooth scrolling
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if( $('#header').length ) {
+          top_space = $('#header').outerHeight();
+
+          if( ! $('#header').hasClass('header-fixed') ) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ( $(this).parents('.nav-menu').length ) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ( $('body').hasClass('mobile-nav-active') ) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
   });
 
   // Initiate superfish on nav menu
@@ -116,4 +140,20 @@ $(document).ready(function() {
     return false;
   });
 
+});
+
+ // Porfolio filter
+ $("#portfolio-flters li").click ( function() {
+  $("#portfolio-flters li").removeClass('filter-active');
+  $(this).addClass('filter-active');
+
+  var selectedFilter = $(this).data("filter");
+  $("#portfolio-wrapper").fadeTo(100, 0);
+
+  $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+
+  setTimeout(function() {
+    $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+    $("#portfolio-wrapper").fadeTo(300, 1);
+  }, 300);
 });
